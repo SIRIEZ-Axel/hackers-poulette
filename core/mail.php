36c287c2152c0e0fdd7538a674a63617b046d969
axel <axel.siriez@gmail.com>
2022-04-29 15:48:53 +0200

    mail:fix

--- core/mail.php
+++ core/mail.php
@@ -3,39 +3,40 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 
+require_once '../core/read.php';
+require_once '../core/conn.php';
+require_once '../core/create.php';
 require_once '../PHPMailer/Exception.php';
 require_once '../PHPMailer/PHPMailer.php';
 require_once '../PHPMailer/SMTP.php';
 
+$adresse_exp =$_GET['mail'];
+$sujet = $_GET['sujet'];
+$message = $_GET['message'];
 
 $mail = new PHPMailer(true);
-try{
 
-    // config
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER; // Je veux des info de debug
+// config
+$mail->SMTPDebug = SMTP::DEBUG_SERVER; // Je veux des info de debug
 
-    // config du SMTP
-    $mail->isSMTP();
-    $mail->Host = 'localhost';
-    $mail->Port = 1025;
-    
+// config du SMTP
+$mail->isSMTP();
+$mail->Host = 'localhost';
+$mail->Port = 1025;
+
 //charset
 $mail->CharSet = "utf-8";
 
 //Destinataire
-$mail->addAddress('exemple@site.fr');
+$mail->addAddress('Hacker-poulette-support@site.com');
 
 //expéditeur
-$mail->setFrom('no-reply@site.fr');
+$mail->setFrom($adresse_exp);
 
 //contenu
-$mail->Subject = "Sujet du message";
-$mail->Body = "message";
+$mail->Subject = $sujet;
+$mail->Body = $message;
 
 //on envoie
 $mail->send();
 echo "message envoyé";
-} catch(Exception){
-    echo 'Message non envoyé';
-}
-
